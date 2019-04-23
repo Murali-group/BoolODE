ee882a1ede0f8eb31a3c4222e3adc11d9026e8eb
Aditya Pratapa <adyprat@csb.cs.vt.edu>
2019-04-23 04:30:10 +0000

    Added script to plot two cluster visualizations

--- src/cluster-vis.py
+++ src/cluster-vis.py
@@ -9,6 +9,9 @@ parser.add_option('-i','--inFile',default='',type=str,
 inFile = opts.inFile
 DF = pd.read_csv(inFile,sep=',',index_col=0)
 genes = DF.loc[[row for row in DF.index if 'p_' not in row]]
-genes.drop([col for col in genes.columns if genes[col].sum() < 0.2*len(genes.index)],axis=1,inplace=True)
+dropped = genes.drop([col for col in genes.columns if genes[col].sum() < 0.2*len(genes.index)],axis=1)
 sns.clustermap(genes)
 plt.savefig(inFile.split('.csv')[0] + '_clustered-genes.png')
+
+sns.clustermap(dropped)
+plt.savefig(inFile.split('.csv')[0] + '_clustered-genes-dropped.png')
