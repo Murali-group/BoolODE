5373b20af102712942c5624993ea91e33ceea755
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-13 16:23:17 -0400

    Update BoolODE_gendatasets.py

--- BoolODE_gendatasets.py
+++ BoolODE_gendatasets.py
@@ -13,7 +13,7 @@ output_dir = "CuratedData/"
 model_name = "mCAD"
 output_name = "mCAD"
 list_of_mCAD_sims = ["mCAD-sim-01-ts-800-cells-1000", "mCAD-sim-02-ts-800-cells-1000", "mCAD-sim-03-ts-800-cells-1000", "mCAD-sim-04-ts-800-cells-1000", "mCAD-sim-05-ts-800-cells-1000", "mCAD-sim-06-ts-800-cells-1000", "mCAD-sim-07-ts-800-cells-1000", "mCAD-sim-08-ts-800-cells-1000", "mCAD-sim-09-ts-800-cells-1000", "mCAD-sim-10-ts-800-cells-1000"]
-for mCAD_sim in list_of_mCAD_sims:
+for mCADsim in list_of_mCAD_sims:
 	echo "Simulating "$model_name
 	python3 $path_to_boolode/src/BoolODE.py --path $path_to_boolode/data/$model_name".txt" \
 						--ics $path_to_boolode/data/$model_name"_ics.txt" \
@@ -27,7 +27,7 @@ for mCAD_sim in list_of_mCAD_sims:
 model_name = "VSC"
 output_name = "VSC"
 list_of_VSC_sims = ["VSC-sim-01-ts-800-cells-1000", "VSC-sim-02-ts-800-cells-1000", "VSC-sim-03-ts-800-cells-1000", "VSC-sim-04-ts-800-cells-1000", "VSC-sim-05-ts-800-cells-1000", "VSC-sim-06-ts-800-cells-1000", "VSC-sim-07-ts-800-cells-1000", "VSC-sim-08-ts-800-cells-1000", "VSC-sim-09-ts-800-cells-1000", "VSC-sim-10-ts-800-cells-1000"]
-for VSC_sim in list_of_VSC_sims:
+for VSCsim in list_of_VSC_sims:
 	echo "Simulating "$model_name
 	python3 $path_to_boolode/src/BoolODE.py --path $path_to_boolode/data/$model_name".txt" \
 						--max-time $maxtime --num-cells $numcells \
@@ -40,7 +40,7 @@ for VSC_sim in list_of_VSC_sims:
 model_name = "HSC"
 output_name = "HSC"
 list_of_HSC_sims = ["HSC-sim-01-ts-800-cells-1000", "HSC-sim-02-ts-800-cells-1000", "HSC-sim-03-ts-800-cells-1000", "HSC-sim-04-ts-800-cells-1000", "HSC-sim-05-ts-800-cells-1000", "HSC-sim-06-ts-800-cells-1000", "HSC-sim-07-ts-800-cells-1000", "HSC-sim-08-ts-800-cells-1000", "HSC-sim-09-ts-800-cells-1000", "HSC-sim-10-ts-800-cells-1000"]
-for HSC_sim in list_of_HSC_sims:
+for HSCsim in list_of_HSC_sims:
 	echo "Simulating "$model_name
 	python3 $path_to_boolode/src/BoolODE.py --path $path_to_boolode/data/$model_name".txt" \
 						--ics $path_to_boolode/data/$model_name"_ics.txt" \
@@ -54,7 +54,7 @@ for HSC_sim in list_of_HSC_sims:
 model_name = "GSD"
 output_name = "GSD"
 list_of_GSD_sims = ["GSD-sim-01-ts-800-cells-1000", "GSD-sim-02-ts-800-cells-1000", "GSD-sim-03-ts-800-cells-1000", "GSD-sim-04-ts-800-cells-1000", "GSD-sim-05-ts-800-cells-1000", "GSD-sim-06-ts-800-cells-1000", "GSD-sim-07-ts-800-cells-1000", "GSD-sim-08-ts-800-cells-1000", "GSD-sim-09-ts-800-cells-1000", "GSD-sim-10-ts-800-cells-1000"]
-for GSD_sim in list_of_GSD_sims:
+for GSDsim in list_of_GSD_sims:
 	echo "Simulating "$model_name
 	python3 $path_to_boolode/src/BoolODE.py --path $path_to_boolode/data/$model_name".txt" \
 						--ics $path_to_boolode/data/$model_name"_ics.txt" \
