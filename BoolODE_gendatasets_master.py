428d8f391b01829546b8d348008e91c9aca23349
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-14 00:18:31 -0400

    Add files via upload

---
+++ BoolODE_gendatasets_master.py
@@ -0,0 +1,37 @@
+#BoolODE_gendatasets_master.py
+
+import argparse
+import sys
+import os
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description='input pathway and output directory')
+    parser.add_argument('inputdir', help='Path to BoolODE')
+    parser.add_argument('--output-dir', help='Path to output files')
+    return parser
+
+if __name__ == "__main__":
+    arg_parser = create_arg_parser()
+    parsed_args = arg_parser.parse_args(sys.argv[1:])
+    if os.path.exists(parsed_args.inputdir):
+      print("File exists")
+
+num_simulations = 10
+num_cells = [250, 500, 1000, 2000, 5000]
+num_timesteps = [100, 200, 400, 800, 1600]
+model_name = 'mCAD'
+
+for cells in num_cells:
+    for ts in num_timesteps:
+        for i in num_simulations:
+            experiment_name = model_name + '-ts-' + str(num_timesteps) + '-cells-' + str(num_cells) + '-sim-' + str(num_simulations)
+            input_file_prefix = inputdir + '/data/' + model_name 
+            # you may have to create the directory corresponding to experiment_name since it may not exist.
+            command = 'python3 BoolODE.py --path ' + input_file_prefix + '.txt' 
+            + '--ics ' + input_file_prefix + '_ics.txt'
+            + '--max-time ' + str(num_timesteps) + ' --num-cells ' + str(num_cells) 
+            + ' --do-parallel '
+            + ' --outPrefix ' + experiment_name + "/sim-" + str(num_simulations) + ".out"
+            + ' --sample-cells'
+            print(command)
+            os.system(command)
