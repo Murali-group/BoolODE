ab5c9c99fad18e3add5716046ff9a86d59bbf389
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-14 01:08:22 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -6,14 +6,14 @@ import os
 
 def create_arg_parser():
     parser = argparse.ArgumentParser(description='input pathway and output directory')
-    parser.add_argument('path-to-BoolODE', help='Path to BoolODE')
+    parser.add_argument('pathtoBoolODE', help='Path to BoolODE')
     parser.add_argument('--output-dir', help='Path to output files')
     return parser
 
 if __name__ == "__main__":
     arg_parser = create_arg_parser()
     parsed_args = arg_parser.parse_args(sys.argv[1:])
-    if os.path.exists(parsed_args.path-to-BoolODE):
+    if os.path.exists(parsed_args.pathtoBoolODE):
       print("File exists")
 
 num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -25,7 +25,7 @@ for cells in num_cells:
     for ts in num_timesteps:
         for i in num_simulations:
             experiment_name = model_name + '-ts-' + str(num_timesteps) + '-cells-' + str(num_cells) + '-sim-' + str(num_simulations)
-            input_file_prefix = path-to-BoolODE + '/data/' + model_name 
+            input_file_prefix = pathtoBoolODE + '/data/' + model_name 
             command = 'python3 BoolODE.py --path ' + input_file_prefix + '.txt' 
             + '--ics ' + input_file_prefix + '_ics.txt'
             + '--max-time ' + str(num_timesteps) + ' --num-cells ' + str(num_cells) 
