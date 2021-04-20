90551c84c96151612183e9835bfdb001006988d7
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-20 14:21:50 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -29,11 +29,11 @@ for cells in num_cells:
         for i in num_simulations:
             experiment_name = model_name + '-ts-' + str(num_timesteps) + '-cells-' + str(num_cells) + '-sim-' + str(num_simulations)
             input_file_prefix = pathtoBoolODE + '/data/' + model_name 
-            command = 'python3 BoolODE.py --path ' + input_file_prefix + '.txt' 
-            + '--ics ' + input_file_prefix + '_ics.txt'
-            + '--max-time ' + str(num_timesteps) + ' --num-cells ' + str(num_cells) 
-            + ' --do-parallel '
-            + ' --outPrefix ' + outputdir + experiment_name + "/sim-" + str(num_simulations) + ".out"
+            command = 'python3 BoolODE.py --path ' + input_file_prefix + '.txt' \
+            + '--ics ' + input_file_prefix + '_ics.txt' \
+            + '--max-time ' + str(num_timesteps) + ' --num-cells ' + str(num_cells) \
+            + ' --do-parallel ' \
+            + ' --outPrefix ' + outputdir + experiment_name + "/sim-" + str(num_simulations) + ".out" \
             + ' --sample-cells'
             print(command)
             os.system(command)
