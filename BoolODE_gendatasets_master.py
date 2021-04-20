6bb08a31fba763fc939a3d333d8660bffd40ed1b
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-20 17:23:57 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 import os
 
-pathtoBoolODE = '/home/cbuck016/BoolODE-0.1/src/'
+pathtoBoolODE = '/home/cbuck016/BoolODE-0.1/'
 outputdir = '/home/cbuck016/BoolODE-0.1/Test1/'
 
 #def create_arg_parser():
@@ -32,7 +32,7 @@ for cells in num_cells:
         for i in num_simulations:
             experiment_name = model_name + '-ts-' + str(ts) + '-cells-' + str(cells) + '-sim-' + str(i)
             input_file_prefix = pathtoBoolODE + 'data/' + model_name 
-            command = 'python3 ' + pathtoBoolODE + 'BoolODE.py --path ' + input_file_prefix + '.txt ' \
+            command = 'python3 ' + pathtoBoolODE + 'src/BoolODE.py --path ' + input_file_prefix + '.txt ' \
             + '--ics ' + input_file_prefix + '_ics.txt ' \
             + '--max-time ' + str(ts) + ' --num-cells ' + str(cells) \
             + ' --do-parallel ' \
