0255f2d26ebbea329cee9e68455d62de3a92fceb
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-27 16:32:44 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -19,9 +19,13 @@ outputdir = '/home/cbuck016/BoolODE-0.1/Test1/'
 #    if os.path.exists(parsed_args.pathtoBoolODE):
 #      print("File exists")
 
-num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-num_cells = [250, 500, 1000, 2000, 5000]
-num_timesteps = [1, 2, 4, 8, 16]
+num_simulations = [1]
+num_cells = [250]
+num_timesteps = [1]
+
+#num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#num_cells = [250, 500, 1000, 2000, 5000]
+#num_timesteps = [1, 2, 4, 8, 16]
 model_name = 'mCAD'
 
 for cells in num_cells:
