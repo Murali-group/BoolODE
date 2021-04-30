5e214714c9fbe5bcd6b3b188e9a6dd96fb485072
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-04-30 09:57:41 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -19,9 +19,12 @@ outputdir = '/home/cbuck016/BoolODE-0.1/GSD-sims/'
 #    if os.path.exists(parsed_args.pathtoBoolODE):
 #      print("File exists")
 
-num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-num_cells = [250, 500, 1000, 2000, 5000]
-num_timesteps = [1, 2, 4, 8, 16]
+num_simulations = [9, 10]
+num_cells = [5000]
+num_timesteps = [16]
+#num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#num_cells = [250, 500, 1000, 2000, 5000]
+#num_timesteps = [1, 2, 4, 8, 16]
 model_name = 'GSD'
 
 for cells in num_cells:
