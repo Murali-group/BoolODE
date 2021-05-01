3993913d3ddbe8c4369caf8394ff2bc2309bbeea
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-05-01 12:49:34 -0400

    Update BoolODE_gendatasets_master.py

--- BoolODE_gendatasets_master.py
+++ BoolODE_gendatasets_master.py
@@ -7,18 +7,6 @@ import os
 pathtoBoolODE = '/home/cbuck016/BoolODE-0.1/'
 outputdir = '/home/cbuck016/BoolODE-0.1/GSD-sims/'
 
-#def create_arg_parser():
-#    parser = argparse.ArgumentParser(description='input pathway and output directory')
-#    parser.add_argument('pathtoBoolODE', help='Path to BoolODE')
-#    parser.add_argument('--output-dir', help='Path to output files')
-#    return parser
-
-#if __name__ == "__main__":
-#    arg_parser = create_arg_parser()
-#    parsed_args = arg_parser.parse_args(sys.argv[1:])
-#    if os.path.exists(parsed_args.pathtoBoolODE):
-#      print("File exists")
-
 num_simulations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 num_cells = [250, 500, 1000, 2000, 5000]
 num_timesteps = [1, 2, 4, 8, 16]
