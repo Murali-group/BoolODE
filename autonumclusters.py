7833ae19c44ad2c8e0847df0e0341d47e5fa4200
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-05-01 22:44:07 -0400

    Update autonumclusters.py

--- autonumclusters.py
+++ autonumclusters.py
@@ -15,6 +15,7 @@ from optparse import OptionParser
 def parseArgs(args):
     parser = OptionParser()
     parser.add_option('', '--expressionfile', type='str', help='Path to ExpressionData.csv file')
+    parser.add_option('', '--outPrefix', type='str', default='', help='Prefix for output files.')
     
     (opts, args) = parser.parse_args(args)
     return opts, args
@@ -23,6 +24,7 @@ def parseArgs(args):
 def main(args):
     opts, args = parseArgs(args)
     expressionfile = opts.expressionfile
+    outPrefix = opts.outPrefix
     if expressionfile is None or len(expressionfile) == 0:
         print("Please specify path to ExpressionData.csv file")
         sys.exit
@@ -30,7 +32,13 @@ def main(args):
         expfileDF = pd.read_csv(expressionfile, sep='\t', engine='python3', index_col=0)
         data = expfileDF.transpose()
         print(data)
-        
+    if len(outPrefix) > 0:
+        if '/' in outPrefix:
+            outDir = '/'.join(outPrefix.split('/')[:-1])
+            if not os.path.exists(outDir):
+                print(outDir, "does not exist, creating it...")
+                os.makedirs(outDir)
+                
         range_n_clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10]
         silhouette_avg_n_clusters = []
         
@@ -47,7 +55,7 @@ def main(args):
         best_num_cluster = silhouette_avg_n_clusters.index(best_avg_silhouette_value)
         
         print("The best average silhouette score is: ", best_avg_silhouette_value)
-        
+        silhouette_avg_n_clusters.to_csv(outPrefix + 'silhouettescores.csv')
 
 #Read ExpressionData.csv file
 #data = pd.read_csv (r'/Users/cbuck016/Desktop/BoolODE-0.1/VSC_5000/ExpressionData.csv', index_col=0)
