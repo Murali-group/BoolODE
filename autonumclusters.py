0c977c20e431ea7410d352bdfd9b4c0d002dea4c
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-05-02 21:51:28 -0400

    Update autonumclusters.py

--- autonumclusters.py
+++ autonumclusters.py
@@ -58,9 +58,9 @@ def main(args):
         
         print("The best average silhouette score is: ", best_avg_silhouette_value)
         
-        df = pd.DataFrame(silhouette_avg_n_clusters)
-        print(df)
-        df.to_csv(outPrefix + 'silhouettescores.csv')
+        #df = pd.DataFrame(silhouette_avg_n_clusters)
+        #print(df)
+        #df.to_csv(outPrefix + 'silhouettescores.csv')
         
         
         
