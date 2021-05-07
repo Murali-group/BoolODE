b971f3b806db3eab8105d9505357fb2a0b26806f
cbuck016 <80789101+cbuck016@users.noreply.github.com>
2021-05-06 22:29:14 -0400

    Update silhouetteanalysis.py

--- silhouetteanalysis.py
+++ silhouetteanalysis.py
@@ -16,41 +16,46 @@ ctr_of_10 = 0
 for cluster in my_reader:
   if cluster[1] == '2':
     ctr_of_2 += 1
-    print("2 clusters predicted " + str(ctr_of_2) + " times")                      
+                        
   for cluster in my_reader:
     if cluster[1] == '3':
       ctr_of_3 += 1
-      print("3 clusters predicted " + str(ctr_of_3) + " times")
+      
     for cluster in my_reader:
       if cluster[1] == '4':
         ctr_of_4 += 1
-        print("4 clusters predicted " + str(ctr_of_4) + " times")                 
+                        
       for cluster in my_reader:
         if cluster[1] == '5':
           ctr_of_5 += 1
-          print("5 clusters predicted " + str(ctr_of_5) + " times")
+          
         for cluster in my_reader:
           if cluster[1] == '6':
             ctr_of_6 += 1
-            print("6 clusters predicted " + str(ctr_of_6) + " times")
+            
           for cluster in my_reader:
             if cluster[1] == '7':
               ctr_of_7 += 1
-              print("7 clusters predicted " + str(ctr_of_7) + " times")     
+                   
             for cluster in my_reader:
               if cluster[1] == '8':
                 ctr_of_8 += 1
-                print("8 clusters predicted " + str(ctr_of_8) + " times")
+                
               for cluster in my_reader:
                 if cluster[1] == '9':
                   ctr_of_9 += 1
-                  print("9 clusters predicted " + str(ctr_of_9) + " times")
+                  
                 for cluster in my_reader:
                   if cluster[1] == '10':
                     ctr_of_10 += 1
-                    print("10 clusters predicted " + str(ctr_of_10) + " times")
+                    
                  
-
-
-                 
-                 
+print("2 clusters predicted " + str(ctr_of_2) + " times") 
+print("3 clusters predicted " + str(ctr_of_3) + " times")
+print("4 clusters predicted " + str(ctr_of_4) + " times") 
+print("5 clusters predicted " + str(ctr_of_5) + " times")                 
+print("6 clusters predicted " + str(ctr_of_6) + " times")                
+print("7 clusters predicted " + str(ctr_of_7) + " times")
+print("8 clusters predicted " + str(ctr_of_8) + " times")
+print("9 clusters predicted " + str(ctr_of_9) + " times")
+print("10 clusters predicted " + str(ctr_of_10) + " times")
